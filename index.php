d9144dd999d539fd82cffb802221f6d60eb56f9e
FRAALES <alessandrofranco1992@gmail.com>
2023-06-22 14:33:47 +0200

    index pronto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Todo
+
+        Dobbiamo creare una web-app che permetta di leggere e scrivere una lista di Todo. 
+        Deve essere anche gestita la persistenza dei dati leggendoli da, e scrivendoli in un file JSON.
+
+        Nello svolgere l’esercizio seguite un approccio graduale:
+        
+        1) Prima assicuratevi che la vostra pagina index (front-end) riesca a comunicare correttamente con il vostro script PHP (back-end API).
+
+        2) Lo step successivo è quello di "testare" l'invio di un nuovo task, sapendo che manca comunque la persistenza lato server (ancora non            memorizzate i dati da nessuna parte).
+
+        3) Solo a questo punto sarà utile passare alla lettura della lista da un file JSON. -->
